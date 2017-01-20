4f7d5374b392c1e44269577d8b2eff10bf0e8b9b
Gordon Byers <gordonbyers@yahoo.com>
2017-01-20 22:52:36 +0000

    Works for entire Workspace collection

--- EmbedSample/Controllers/DashboardController.cs
+++ EmbedSample/Controllers/DashboardController.cs
@@ -13,14 +13,14 @@ namespace paas_demo.Controllers
     public class DashboardController : Controller
     {
         private readonly string workspaceCollection;
-        private readonly string workspaceId;
+        //private readonly string workspaceId;
         private readonly string accessKey;
         private readonly string apiUrl;
 
         public DashboardController()
         {
             this.workspaceCollection = ConfigurationManager.AppSettings["powerbi:WorkspaceCollection"];
-            this.workspaceId = ConfigurationManager.AppSettings["powerbi:WorkspaceId"];
+            //this.workspaceId = ConfigurationManager.AppSettings["powerbi:WorkspaceId"];
             this.accessKey = ConfigurationManager.AppSettings["powerbi:AccessKey"];
             this.apiUrl = ConfigurationManager.AppSettings["powerbi:ApiUrl"];
         }
@@ -35,24 +35,43 @@ namespace paas_demo.Controllers
         {
             using (var client = this.CreatePowerBIClient())
             {
-                var reportsResponse = client.Reports.GetReports(this.workspaceCollection, this.workspaceId);
-
-                var viewModel = new ReportsViewModel
+                var workspaces = client.Workspaces.GetWorkspacesByCollectionName(this.workspaceCollection);
+                var viewModel = new WorkspaceReportsViewModel() { WorkspaceReports = new System.Collections.Generic.List<ReportsViewModel>() };
+                
+                foreach (var workspace in workspaces.Value)
                 {
-                    Reports = reportsResponse.Value.ToList()
-                };
+                    var reportsResponse = client.Reports.GetReports(this.workspaceCollection, workspace.WorkspaceId);
+
+                    //var t1 = new ReportsViewModel();
+                    //t1.Reports = reportsResponse.Value.ToList();
+                    //t1.WorkspaceId = workspace.WorkspaceId;
+                    //viewModel.WorkspaceReports.Add(t1);
+
+                    //var t2 = new ReportsViewModel()
+                    //{
+                    //    Reports = reportsResponse.Value.ToList(),
+                    //    WorkspaceId = workspace.WorkspaceId
+                    //};
+                    //viewModel.WorkspaceReports.Add(t2);
+
+                    viewModel.WorkspaceReports.Add( new ReportsViewModel()
+                    {
+                        Reports = reportsResponse.Value.ToList(),
+                        WorkspaceId = workspace.WorkspaceId
+                    });
+                }
 
                 return PartialView(viewModel);
             }
         }
 
-        public async Task<ActionResult> Report(string reportId)
+        public async Task<ActionResult> Report(string reportId, string workspaceId)
         {
             using (var client = this.CreatePowerBIClient())
             {
-                var reportsResponse = await client.Reports.GetReportsAsync(this.workspaceCollection, this.workspaceId);
+                var reportsResponse = await client.Reports.GetReportsAsync(this.workspaceCollection, workspaceId);
                 var report = reportsResponse.Value.FirstOrDefault(r => r.Id == reportId);
-                var embedToken = PowerBIToken.CreateReportEmbedToken(this.workspaceCollection, this.workspaceId, report.Id);
+                var embedToken = PowerBIToken.CreateReportEmbedToken(this.workspaceCollection, workspaceId, report.Id);
 
                 var viewModel = new ReportViewModel
                 {
--- EmbedSample/Models/ReportsViewModel.cs
+++ EmbedSample/Models/ReportsViewModel.cs
@@ -6,5 +6,11 @@ namespace paas_demo.Models
     public class ReportsViewModel
     {
         public List<Report> Reports { get; set; }
+        public string WorkspaceId { get; set; }
+    }
+
+    public class WorkspaceReportsViewModel
+    {
+        public List<ReportsViewModel> WorkspaceReports { get; set; }
     }
 }
